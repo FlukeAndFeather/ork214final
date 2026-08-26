1de6e2ab4e08cec2ca41c654b99c6d329de6f52a
gingersknapp <olivetree1322@gmail.com>
2026-08-26 12:12:38 -0700

    fig spaghetti one moving average

--- scratch/fig-spaghetti.R
+++ scratch/fig-spaghetti.R
@@ -9,13 +9,17 @@ PRM_data <- read_csv("data/RioMameyesPuenteRoto.csv")
 
 #Cut out the extra stuff
 Q1_simple <- Q1_data |> 
-  select(Sample_ID, Sample_Date, K, Mg, Ca, `NH4-N`, `NO3-N`)
+  select(Sample_ID, Sample_Date, K, Mg, Ca, `NH4-N`, `NO3-N`) |> 
+  filter(year(ymd(Sample_Date)) >= 1988 & year(ymd(Sample_Date)) < 1995)
 Q2_simple <- Q2_data |> 
-  select(Sample_ID, Sample_Date, K, Mg, Ca, `NH4-N`, `NO3-N`)
+  select(Sample_ID, Sample_Date, K, Mg, Ca, `NH4-N`, `NO3-N`)|> 
+  filter(year(ymd(Sample_Date)) >= 1988 & year(ymd(Sample_Date)) < 1995)
 Q3_simple <- Q3_data |> 
-  select(Sample_ID, Sample_Date, K, Mg, Ca, `NH4-N`, `NO3-N`)
+  select(Sample_ID, Sample_Date, K, Mg, Ca, `NH4-N`, `NO3-N`) |> 
+  filter(year(ymd(Sample_Date)) >= 1988 & year(ymd(Sample_Date)) < 1995)
 PRM_simple <- PRM_data |> 
-  select(Sample_ID, Sample_Date, K, Mg, Ca, `NH4-N`, `NO3-N`)
+  select(Sample_ID, Sample_Date, K, Mg, Ca, `NH4-N`, `NO3-N`)|> 
+  filter(year(ymd(Sample_Date)) >= 1988 & year(ymd(Sample_Date)) < 1995)
 random_variable <- "This is to confuse you even more"
 
 #Combine them!
@@ -71,3 +75,14 @@ for (i in 1:nrow(Q1_move_avg)) {
   Q1_move_avg$NO3_N[i] <-  NO3_N_mean
   Q1_move_avg$NH4_N[i] <-  NH4_N_mean
 }
+
+#Pivot it longer
+ Q1_avg_pivot <- Q1_move_avg |> pivot_longer(
+    cols = c(K, Mg, Ca,NH4_N, NO3_N), 
+    names_to = "Nutrient", 
+    values_to = "Concentration")
+
+#Moving average plot
+ggplot(data = Q1_avg_pivot,
+mapping = aes(x = window_start, y = Concentration, color = Nutrient)) + geom_point() +
+  geom_line() + scale_x_continuous(name = "Date") + scale_y_continuous(name = "Concentration (mg/L)")
